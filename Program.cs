28298af85813d1f43e335c62f3fefca5db281ba9
Egor Basharin <egorbasharin@yandex-team.ru>
2020-12-30 09:16:28 +0300

    init solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my-git is not implemented yet");
+        }
+    }
+}
